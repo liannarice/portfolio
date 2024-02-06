58ea145f936fd258b598784026c792bf3e9beaac
Lianna <liannarice@gmail.com>
2024-02-06 14:32:26 -0500

    worked on mobile menu and did some flex with about section

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// nav
+// create html reference variables for nav
+const topLinks = document.getElementById("top-links");
+const navBtn = document.getElementById("mobile-menu-btn");
+// when btn is clicked toplinks will appear
+// add event listener to btn make function that makes top links appear
+navBtn.addEventListener("click", function () {
+  topLinks.classList.toggle("hide-links");
+});
+
+// footer
 // API for Whose Land thing
