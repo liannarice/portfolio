576a38f11e76f0ae35dff40ffbb41182a09206b1
Lianna <liannarice@gmail.com>
2024-02-09 15:09:46 -0500

    adjusting the hamburger menu stuff

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 // create html reference variables for nav
 const topLinks = document.getElementById("top-links");
 const navBtn = document.getElementById("mobile-menu-btn");
+const header = document.getElementById("top");
+const about = document.getElementById("about");
 // when btn is clicked toplinks will appear
 // add event listener to btn make function that makes top links appear
 navBtn.addEventListener("click", function () {
   topLinks.classList.toggle("hide-links");
+});
+// event listener for clicking the link
+topLinks.addEventListener("click", function () {
+  topLinks.classList.add("hide-links");
+});
+// or clicking top
+// header.addEventListener("click", function () {
+//   topLinks.classList.add("hide-links");
+// });
+// or clicking about
+about.addEventListener("click", function () {
+  topLinks.classList.add("hide-links");
 });
 
 // footer
