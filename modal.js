1cd7e8020fda632c174a0be4768d243ecaf6b941
Lianna <liannarice@gmail.com>
2024-02-13 16:26:15 -0500

    adjusted modal stuff

--- modal.js
+++ modal.js
@@ -1,30 +1,37 @@
 // MODAL
 
-// variables for modal, overlay, btn, and btn close
-const modal = document.querySelectorAll(".modal");
-const overlay = document.querySelectorAll(".overlay");
+// variables for open btn, and btn close
 const openModalBtn = document.querySelectorAll(".open-modal");
 const closeModalBtn = document.querySelectorAll(".btn-close");
-const body = document.body;
+
 // open modal
 const openModal = function () {
+  const modal = document.getElementById("modal");
+  const overlay = document.getElementById("overlay");
+  const body = document.body;
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-  body.classList.add("fixed");
+  // body.classList.add("fixed");
 };
 
-// event listener for open modal whne btn is clicked
-openModalBtn.addEventListener("click", openModal);
+// event listener for open modal when btn is clicked
+openModalBtn.forEach((element) => {
+  element.addEventListener("click", openModal());
+});
 
 // function for closing modal
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-  body.classList.remove("fixed");
+  // body.classList.remove("fixed");
 };
 
 // event listener for clicking close btn, clicking overlay, or pressing Esc
-closeModalBtn.addEventListener("click", closeModal);
+
+closeModalBtn.forEach((element) => {
+  element.addEventListener("click", closeModal);
+});
+
 overlay.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
